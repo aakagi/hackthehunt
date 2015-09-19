71601ddfc3fc4f52b668320f6feb0576d291aa87
aakagi <aakagi@purdue.edu>
2015-09-19 19:30:58 -0400

    css shit

--- server/methods.js
+++ server/methods.js
@@ -1,15 +1,5 @@
 
-var teamPoints = function(team) {
-    var teamUsers = HtnUsers.find({team: team});    
 
-    var total = 0;
-
-    for (var i = 0; i < teamUsers.length; i++) {
-        var teamUser = teamUsers[i];
-        total += teamUser.points;
-    }
-    return total;
-}
 
 Meteor.methods({
     newPing: function(message) {
@@ -17,9 +7,42 @@ Meteor.methods({
     },
     newUser: function(htnId, email, token, first, last) {
         
-        // var chooseTeam = function() {
+        var teamPoints = function(team) {
+            var teamUsers = HtnUsers.find({team: team});    
 
-        // }
+            var total = 0;
+
+            for (var i = 0; i < teamUsers.length; i++) {
+                var teamUser = teamUsers[i];
+                total += teamUser.points;
+            }
+            return total;
+        }
+
+        var chooseTeam = function() {
+            var redPoints = teamPoints("red");
+            var bluePoints = teamPoints("blue");
+            var yellowPoints = teamPoints("yellow");
+
+            if (redPoints > bluePoints && redPoints > yellowPoints) {
+                return "red";
+            }
+            if (bluePoints > redPoints && bluePoints > yellowPoints) {
+                return "blue";
+            }
+            if (yellowPoints > bluePoints && yellowPoints > redPoints) {
+                return "yellow";
+            }
+            else {
+                var random = Math.random()
+                if (random > 0.666666) return "blue";
+                else if (random < 0.333333) return "yellow";
+                else return "red";
+            }
+        }
+        var chosenTeam = chooseTeam();
+
+        console.log(chosenTeam);
 
         HtnUsers.insert({
             htnId: htnId,
@@ -28,7 +51,7 @@ Meteor.methods({
             first: first,
             last: last,
             points: 10,
-            team: "blue"
+            team: chosenTeam
         });
     }
 
